bb7776fc86193b29e4cd2189708f6bb56edbaf20
Lerrel Pinto <lerrel.pinto@gmail.com>
2018-06-10 16:32:29 -0700

    Added new environments and edited reacher for consistency with the OpenAI Gym framework

--- gym_dart/envs/__init__.py
+++ gym_dart/envs/__init__.py
@@ -1,3 +1,4 @@
 from gym_dart.envs.dart_env import DartEnv
 from gym_dart.envs.dart_cartpole_env import DartCartPoleEnv
 from gym_dart.envs.dart_reacher_env import DartReacherEnv
+from gym_dart.envs.dart_particle_env import DartParticleEnv
--- gym_dart/envs/dart_particle_env.py
+++ gym_dart/envs/dart_particle_env.py
@@ -4,38 +4,38 @@ import numpy.linalg as la
 from gym_dart.envs import DartEnv
 import gym.spaces as spaces
 
-class DartReacherEnv(DartEnv):
+class DartParticleEnv(DartEnv):
 
     def __init__(self):
-        DartEnv.__init__(self, world_path='reacher.skel')
+        DartEnv.__init__(self, world_path='particle.skel')
 
         self.world.setGravity(np.array([0, 0, -9.8]))
-        self.world.setTimeStep(0.01)
+        self.world.setTimeStep(0.01) # FUTURE: make this an input parameter
+        self.max_time_steps = 1000  # FUTURE: make this an input parameter
 
-        self.reacher = self.world.getSkeleton('reacher')
-        assert self.reacher is not None
-        self.finger_tip = self.reacher.getBodyNode("fingertip")
+        self.particle_skeleton = self.world.getSkeleton('particle')
+        assert self.particle_skeleton is not None
+        self.particle = self.particle_skeleton.getRootBodyNode()
+        assert self.particle is not None
 
         self.target_skeleton = self.world.getSkeleton('target')
         assert self.target_skeleton is not None
         self.target = self.target_skeleton.getRootBodyNode()
         assert self.target is not None
 
-        self.max_time_steps = 1000
         self.current_step = 0
 
         # Observations
-        self.obs_dim = 10
+        self.obs_dim = 8
         self.obs_max = np.array([
-            1.0, 1.0,  # cos(joint_angles_of_reacher)
-            1.0, 1.0,  # sin(joint_angles_of_reacher)
+            0.2, 0.2,  # XY_position_of_particle
+            5.0, 5.0,  # XY_velocity_of_particle
             0.2, 0.2,  # XY_position_of_target
-            5, 5,      # joint_velocities_of_reacher
-            0.4, 0.4,  # com_of_fingertip - com_of_target
+            1.0, 1.0,  # diff_positions
         ])
         self.obs_min = -self.obs_max
-        self.initial_reacher_pos_lower_limits = np.ones(2) * -0.1
-        self.initial_reacher_pos_upper_limits = np.ones(2) * 0.1
+        self.initial_particle_pos_lower_limits = np.ones(2) * -0.2
+        self.initial_particle_pos_upper_limits = np.ones(2) * 0.2
         self.initial_target_pos_lower_limits = np.ones(2) * -0.2
         self.initial_target_pos_upper_limits = np.ones(2) * 0.2
         self.observation_space = spaces.Box(low=self.obs_min, high=self.obs_max)
@@ -48,7 +48,7 @@ class DartReacherEnv(DartEnv):
 
         # Action
         self.action_dim = 2
-        self.actuator_upper_limits = np.ones(self.action_dim) * 0.5
+        self.actuator_upper_limits = np.ones(self.action_dim) * 1
         self.actuator_lower_limits = -self.actuator_upper_limits
         self.action_space = spaces.Box(low=self.actuator_lower_limits, high=self.actuator_upper_limits)
 
@@ -67,19 +67,15 @@ class DartReacherEnv(DartEnv):
         self.world.reset()
         self.current_step = 0
 
-        reacher_pos \
-            = np.random.uniform(self.initial_reacher_pos_lower_limits, self.initial_reacher_pos_upper_limits)
-        self.reacher.setPositions(reacher_pos)
-        self.reacher.resetVelocities()
-        self.reacher.resetAccelerations()
-
+        particle_pos \
+            = np.random.uniform(self.initial_particle_pos_lower_limits, self.initial_particle_pos_upper_limits)
+        self.particle_skeleton.setPosition(0, particle_pos[0])
+        self.particle_skeleton.setPosition(1, particle_pos[1])
+        self.particle_skeleton.resetVelocities()
+        self.particle_skeleton.resetAccelerations()
+        
         target_pos \
             = np.random.uniform(self.initial_target_pos_lower_limits, self.initial_target_pos_upper_limits)
-        while True:
-            target_pos \
-                = np.random.uniform(self.initial_target_pos_lower_limits, self.initial_target_pos_upper_limits)
-            if la.norm(target_pos)<0.2:
-                break
         self.target_skeleton.setPosition(0, target_pos[0])
         self.target_skeleton.setPosition(1, target_pos[1])
 
@@ -89,36 +85,42 @@ class DartReacherEnv(DartEnv):
         pass
 
     def _get_obs(self):
-        # References: https://github.com/openai/gym/blob/v0.10.5/gym/envs/mujoco/reacher.py#L35-L43
-        reacher_pos = self.reacher.getPositions()
-        reacher_vel = self.reacher.getVelocities()
-        target_pose = self.target.getTransform()
-        target_xy = target_pose[:2, 3]
+        particle_xy_pos = self.particle_skeleton.getPositions()[:2,-1]
+        particle_xy_vel = self.particle_skeleton.getVelocities()[:2,-1]
+        target_xy_pos = self.target_skeleton.getPositions()[:2,-1]
         target_com = self.target.getCOM()
-        finger_tip_com = self.finger_tip.getCOM()
-
+        particle_com = self.particle.getCOM()
+        diff_com = (target_com - particle_com).flatten()[:2]
+        self.diff_norm = la.norm(diff_com)
         return np.concatenate([
-            np.cos(reacher_pos).flatten(),
-            np.sin(reacher_pos).flatten(),
-            target_xy.flatten(),
-            reacher_vel.flatten(),
-            (finger_tip_com - target_com).flatten()[:2]
+            particle_xy_pos.flatten(),
+            particle_xy_vel.flatten(),
+            target_xy_pos.flatten(),
+            diff_com.flatten(),
         ])
 
     def _step_world(self, action):
         if self._is_done():
             return 0
+        action = np.clip(action, self.actuator_lower_limits, self.actuator_upper_limits)
         action = np.array(action).astype(float)
-        self.reacher.setCommands(action)
+        action = np.concatenate([action, np.array([0.0])])
+        self.particle_skeleton.setCommands(action)
         self.world.step()
 
     def _is_done(self):
-        return self.current_step >= self.max_time_steps
+        if self.current_step >= self.max_time_steps:
+            return True
+        elif self.diff_norm > 1.0:
+            # incase particle flies away
+            return True
+        else:
+            return False
 
     def _get_reward(self, action):
-        fingertip_com = self.finger_tip.getCOM()
+        particle_com = self.particle.getCOM()
         target_com = self.target.getCOM()
-        diff = (fingertip_com - target_com).flatten()[:2]
+        diff = (particle_com - target_com).flatten()[:2]
 
         reward_distance = -la.norm(diff)
         reward_control = -la.norm(action)
