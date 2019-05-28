d615c4b73610dc1ab42c18d4e094ee9a48505e15
Jeongseok Lee <jslee02@gmail.com>
2019-05-27 23:38:42 -0700

    Update

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,7 +7,6 @@ sudo apt-get install -y build-essential cmake pkg-config git
 sudo apt-get install -y libeigen3-dev libassimp-dev libccd-dev libfcl-dev libboost-regex-dev libboost-system-dev
 sudo apt-get install -y libtinyxml2-dev liburdfdom-dev
 sudo apt-get install -y libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev
-sudo apt-get install -y pybind11-dev
 if [ $(lsb_release -sc) = "xenial" ]; then
   git clone https://github.com/pybind/pybind11 -b 'v2.2.4' --single-branch --depth 1
   cd pybind11
