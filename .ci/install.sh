7e1a9e436c3f79015b626938ea9a5015b161afe8
Jeongseok Lee <jslee02@gmail.com>
2019-05-27 22:46:58 -0700

    Update azure script

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,29 +2,6 @@
 
 set -ex
 
-# DART required dependencies
-sudo apt-get install build-essential cmake pkg-config git
-sudo apt-get install libeigen3-dev libassimp-dev libccd-dev libfcl-dev libboost-regex-dev libboost-system-dev
-sudo apt-get install libtinyxml2-dev liburdfdom-dev
-sudo apt-get install libxi-dev libxmu-dev freeglut3-dev libopenscenegraph-dev
-
-# DART optional dependencies
-sudo apt-get install libbullet-dev libode-dev liboctomap-dev
-if [ $(lsb_release -sc) = "xenial" ]; then
-  sudo apt-get install libnlopt-dev
-elif [ $(lsb_release -sc) = "bionic" ]; then
-  sudo apt-get install libnlopt-dev
-elif [ $(lsb_release -sc) = "cosmic" ]; then
-  sudo apt-get install libnlopt-cxx-dev
-elif [ $(lsb_release -sc) = "disco" ]; then
-  sudo apt-get install libnlopt-cxx-dev
-elif [ $(lsb_release -sc) = "eoan" ]; then
-  sudo apt-get install libnlopt-cxx-dev
-else
-  echo -e "$(lsb_release -sc) is not supported."
-  exit 1
-fi
-
 # Install dartpy
 if [ $(lsb_release -sc) = "trusty" ]; then
   sudo add-apt-repository ppa:libccd-debs/ppa -y
