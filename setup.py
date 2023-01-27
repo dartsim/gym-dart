7d2a4f1d967783af9e355ab75df2c4ca33642084
Jeongseok Lee <jslee02@gmail.com>
2023-01-27 07:35:16 -0800

    Remove .travis.yml

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ from setuptools import setup
 
 setup(name='gym_dart',
       version='0.0.1',
-      install_requires=['gym', 'numpy', 'dartpy', 'pytest']  # And any other dependencies dartpy needs
-)
+      # And any other dependencies dartpy needs
+      install_requires=['gym', 'numpy', 'dartpy', 'pytest']
+      )
