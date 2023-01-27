784c7d271c29d376120a24c0bf48ee7e92186a10
Jeongseok Lee <jslee02@gmail.com>
2023-01-27 07:44:04 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='gym_dart',
-      version='0.0.1',
-      # And any other dependencies dartpy needs
-      install_requires=['gym', 'numpy', 'dartpy', 'pytest']
-      )
+setup(
+    name='gym-dart',
+    version='0.0.1',
+    install_requires=[
+        'dartpy', 'gym', 'numpy', 'pytest'
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
