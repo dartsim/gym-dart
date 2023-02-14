666d925ad9c4559a8368b415fd8bc887675dc588
Jeongseok Lee <jslee02@gmail.com>
2023-02-13 20:34:44 -0800

    Add long_desc

--- setup.py
+++ setup.py
@@ -3,10 +3,16 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="gym-dart",
     version="0.0.1",
-    install_requires=["dartpy", "gymnasium", "numpy", "pytest"],
+    description="A Gym environment wrapper for the DART physics engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["dartpy", "gym", "numpy", "pytest"],
     packages=find_packages(),
     include_package_data=True,
 )
